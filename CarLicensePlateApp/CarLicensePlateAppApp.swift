ae429cabc0c6bb4b671e4b14a9a4a09ecf5e5060
malakhoviv <malakhoviv@osbank.ru>
2023-05-02 20:16:50 +0300

    Initial Commit

---
+++ CarLicensePlateApp/CarLicensePlateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarLicensePlateAppApp.swift
+//  CarLicensePlateApp
+//
+//  Created by Илья Малахов on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarLicensePlateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
